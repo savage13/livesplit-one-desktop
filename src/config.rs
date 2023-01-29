e6088ca08fc657f9e5e05ea89afc6f672b8e623d
Brian Savage <savage13@gmail.com>
2023-01-29 14:03:45 -0500

    Update to key bindings and timer state in livesplit-core

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,21 @@
 use crate::stream_markers;
+use livesplit_core::hotkey::Hotkey;
 use livesplit_core::{
     auto_splitting,
     layout::{self, Layout, LayoutSettings},
     run::{parser::composite, saver::livesplit::save_timer},
     HotkeyConfig, HotkeySystem, Run, Segment, SharedTimer, Timer, TimingMethod,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::{
     fmt, fs,
     io::Cursor,
     path::{Path, PathBuf},
-    time::{Duration, SystemTime},
+    time::SystemTime,
 };
 
-#[derive(Default, Deserialize)]
+#[derive(Default, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Config {
     #[serde(default)]
@@ -25,9 +27,14 @@ pub struct Config {
     hotkeys: HotkeyConfig,
     #[serde(default)]
     connections: Connections,
+    #[serde(default)]
+    keys: HashMap<String, Hotkey>,
+}
+fn default_state_file() -> PathBuf {
+    PathBuf::from("livesplit_state.lsz")
 }
 
-#[derive(Default, Deserialize)]
+#[derive(Default, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 struct General {
     splits: Option<PathBuf>,
@@ -36,9 +43,11 @@ struct General {
     comparison: Option<String>,
     auto_splitter: Option<PathBuf>,
     use_global_hotkeys: Option<bool>,
+    #[serde(default = "default_state_file")]
+    state_file: PathBuf,
 }
 
-#[derive(Default, Deserialize)]
+#[derive(Default, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 struct Log {
     path: PathBuf,
@@ -47,7 +56,7 @@ struct Log {
     clear: bool,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 #[serde(default)]
 struct Window {
@@ -58,7 +67,7 @@ struct Window {
     fps: f32,
 }
 
-#[derive(Default, Deserialize)]
+#[derive(Default, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 #[serde(default)]
 struct Connections {
@@ -77,18 +86,44 @@ impl Default for Window {
     }
 }
 
+impl Window {
+    pub fn size(&self) -> [usize; 2] {
+        [self.width, self.height]
+    }
+}
+
 impl Config {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+    pub fn save_config(&self) {
+        let data = serde_yaml::to_string(self).unwrap();
+        fs::write("config_save.yaml", data).unwrap();
+    }
+    pub fn state_file(&self) -> PathBuf {
+        self.general.state_file.clone()
+    }
+    pub fn set_state_file(&mut self, path: &PathBuf) {
+        self.general.state_file = path.clone()
+    }
     pub fn parse(path: impl AsRef<Path>) -> Option<Config> {
         let buf = fs::read(path).ok()?;
         serde_yaml::from_slice(&buf).ok()
     }
-
+    pub fn window_size(&self) -> [usize; 2] {
+        self.window.size()
+    }
     pub fn parse_run(&self) -> Option<Run> {
         let path = self.general.splits.clone()?;
         let file = fs::read(&path).ok()?;
         let mut run = composite::parse(&file, Some(&path)).ok()?.run;
         run.fix_splits();
         Some(run)
+    }
+    pub fn set_use_global_hotkeys(&mut self, on: bool) {
+        self.general.use_global_hotkeys = Some(on);
     }
     pub fn use_global_hotkeys(&self) -> bool {
         self.general.use_global_hotkeys.unwrap_or(false)
@@ -119,10 +154,12 @@ impl Config {
     pub fn parse_layout_or_default(&self) -> Layout {
         self.parse_layout().unwrap_or_else(Layout::default_layout)
     }
-
-    // pub fn set_splits_path(&mut self, path: PathBuf) {
-    //     self.general.splits = Some(path);
-    // }
+    pub fn splits_path(&self) -> Option<PathBuf> {
+        self.general.splits.clone()
+    }
+    pub fn set_splits_path(&mut self, path: &PathBuf) {
+        self.general.splits = Some(path.clone());
+    }
 
     pub fn create_hotkey_system(&self, timer: SharedTimer) -> Option<HotkeySystem> {
         HotkeySystem::with_config(timer, self.hotkeys).ok()
@@ -179,8 +216,8 @@ impl Config {
             }
         }
     }
-
-    pub fn build_window(&self) -> Result<minifb::Window, minifb::Error> {
+    /*
+    pub fn _build_window(&self) -> Result<minifb::Window, minifb::Error> {
         let mut window = minifb::Window::new(
             "LiveSplit One",
             self.window.width,
@@ -198,7 +235,7 @@ impl Config {
 
         Ok(window)
     }
-
+    */
     pub fn build_marker_client(&self) -> stream_markers::Client {
         stream_markers::Client::new(self.connections.twitch.as_deref())
     }
@@ -209,6 +246,49 @@ impl Config {
                 log::error!("Auto Splitter failed to load: {}", ErrorChain(&e));
             }
         }
+    }
+
+    pub fn reset_key(&self) -> Hotkey {
+        self.hotkeys.reset.unwrap_or("Numpad3".parse().unwrap())
+    }
+    pub fn split_key(&self) -> Hotkey {
+        self.hotkeys.split.unwrap_or("Numpad1".parse().unwrap())
+    }
+    pub fn undo_key(&self) -> Hotkey {
+        self.hotkeys.undo.unwrap_or("Numpad8".parse().unwrap())
+    }
+    pub fn skip_key(&self) -> Hotkey {
+        self.hotkeys.skip.unwrap_or("Numpad2".parse().unwrap())
+    }
+    pub fn pause_key(&self) -> Hotkey {
+        self.hotkeys.pause.unwrap_or("Numpad5".parse().unwrap())
+    }
+    pub fn undo_all_key(&self) -> Hotkey {
+        self.hotkeys
+            .undo_all_pauses
+            .unwrap_or("Numpad7".parse().unwrap())
+    }
+    pub fn prev_key(&self) -> Hotkey {
+        self.hotkeys
+            .previous_comparison
+            .unwrap_or("Numpad4".parse().unwrap())
+    }
+    pub fn next_key(&self) -> Hotkey {
+        self.hotkeys
+            .next_comparison
+            .unwrap_or("Numpad6".parse().unwrap())
+    }
+    pub fn toggle_timing_method_key(&self) -> Hotkey {
+        self.hotkeys
+            .toggle_timing_method
+            .unwrap_or("Numpad9".parse().unwrap())
+    }
+    pub fn keys(&self) -> &HashMap<String, Hotkey> {
+        &self.keys
+    }
+    pub fn set_comparison(&mut self, comparison: &str) {
+        self.general.comparison = Some(comparison.to_string());
+        self.save_config();
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,187 +1,352 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod config;
+pub mod keys;
 mod stream_markers;
 
-use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use config::Config;
+use keys::Key;
 
-use crate::config::Config;
 use bytemuck::{Pod, Zeroable};
-use livesplit_core::{
-    auto_splitting, layout::Component, layout::LayoutState, rendering::software::Renderer, Timer,
-};
-use mimalloc::MiMalloc;
-use minifb::{Key, KeyRepeat};
+use lazy_static::lazy_static;
+use livesplit_core::{auto_splitting, rendering::software::Renderer};
+use livesplit_core::{layout::Layout, layout::LayoutState};
+use livesplit_core::{SharedTimer, Timer};
+use rfd::FileDialog;
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+use winit::event::ElementState;
+use winit::event::KeyboardInput;
+use winit::event::ModifiersState;
+use winit::event::MouseScrollDelta;
+use winit::event::VirtualKeyCode;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
 
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+lazy_static! {
+    static ref CONFIG: RwLock<Config> = RwLock::new(Config::new());
+}
 
-struct WTimer(Arc<RwLock<Timer>>);
+struct WTimer {
+    timer: SharedTimer,
+    markers: stream_markers::Client,
+    layout: Layout,
+    keys: HashMap<Key, Action>,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Action {
+    Split,
+    Reset,
+    Undo,
+    Skip,
+    Pause,
+    UndoAllPauses,
+    PreviousComparison,
+    NextComparison,
+    ToggleTimingMethod,
+    HideComparison,
+    OpenSplits,
+    SaveSplits,
+    Quit,
+    LayoutUp,
+    LayoutDown,
+}
+
+pub fn save_state(timer_state: &livesplit_core::TimerState) {
+    let path = config().state_file();
+    std::fs::write(&path, timer_state.to_json()).unwrap();
+}
 
 impl WTimer {
-    pub fn save_state(&self) {
-        let timer_state = self.read().timer_state();
-        std::fs::write("timer_state.json", timer_state.to_json()).unwrap();
-    }
-
     pub fn load_state(&mut self) {
-        if let Some(timer_state) = livesplit_core::TimerState::from_file("timer_state.json") {
-            println!("loading timer state in timer_state.json");
-            self.write().replace_state(&timer_state);
+        let path = config().state_file();
+        if let Some(path_str) = path.to_str() {
+            if let Some(timer_state) = livesplit_core::TimerState::from_file(path_str) {
+                self.write().replace_state(&timer_state);
+            }
         }
     }
 
     pub fn reset(&mut self) {
         self.write().reset(true);
-        //self.save_state();
     }
     pub fn skip_split(&mut self) {
         self.write().skip_split();
-        //self.save_state();
     }
     pub fn undo_split(&mut self) {
         self.write().undo_split();
-        //self.save_state();
     }
     pub fn pause(&mut self) {
         self.write().toggle_pause();
-        //self.save_state();
     }
     pub fn split_or_start(&mut self) {
         self.write().split_or_start();
-        //self.save_state();
     }
     pub fn switch_to_next_comparison(&mut self) {
         self.write().switch_to_next_comparison();
+        config_mut().set_comparison(self.read().current_comparison());
     }
     pub fn switch_to_previous_comparison(&mut self) {
         self.write().switch_to_previous_comparison();
+        config_mut().set_comparison(self.read().current_comparison());
     }
-    pub fn turn_off_comparison(&mut self) {
+    pub fn turn_off_comparisons(&mut self) {
         self.write().set_current_comparison("None").unwrap();
+        config_mut().set_comparison(self.read().current_comparison());
     }
-
+    pub fn toggle_timing_method(&mut self) {
+        //self.write().set_current_comparison("None").unwrap();
+    }
     fn write(&self) -> RwLockWriteGuard<'_, Timer> {
-        self.0.write().unwrap()
+        self.timer.write().unwrap()
     }
 
     fn read(&self) -> RwLockReadGuard<'_, Timer> {
-        self.0.read().unwrap()
+        self.timer.read().unwrap()
+    }
+    fn new_from_splits(splits_file: PathBuf) -> Self {
+        let mut splits_file = splits_file;
+        config_mut().set_splits_path(&splits_file);
+        splits_file.set_extension("lsz");
+        config_mut().set_state_file(&splits_file);
+        Self::new()
+    }
+    fn new() -> Self {
+        let markers = config().build_marker_client();
+        let layout = config().parse_layout_or_default();
+        let run = config().parse_run_or_default();
+        let timer = Timer::new(run).unwrap().into_shared();
+        let auto_splitter = auto_splitting::Runtime::new(timer.clone());
+        let keys = Self::hotkey_setup(&timer);
+        config().maybe_load_auto_splitter(&auto_splitter);
+        config().configure_timer(&mut timer.write().unwrap());
+        timer.write().unwrap().set_on_timer_change(save_state);
+        Self {
+            timer,
+            markers,
+            layout,
+            keys,
+        }
+    }
+    fn hotkey_setup(timer: &SharedTimer) -> HashMap<Key, Action> {
+        let use_global_hotkeys = config().use_global_hotkeys();
+        let mut keys: HashMap<Key, Action> = HashMap::new();
+
+        let list = [
+            ("open", "Ctrl+O", Action::OpenSplits),
+            ("save", "Ctrl+S", Action::SaveSplits),
+            ("quit", "Ctrl+Q", Action::Quit),
+            ("hide", "Ctrl+T", Action::HideComparison),
+            ("layout_up", "ArrowUp", Action::LayoutUp),
+            ("layout_down", "ArrowDown", Action::LayoutDown),
+        ];
+        for (name, dcmd, action) in list {
+            let key = config()
+                .keys()
+                .get(name)
+                .unwrap_or(&dcmd.parse().unwrap())
+                .into();
+            keys.insert(key, action);
+        }
+        if use_global_hotkeys {
+            let _hotkey_system = config().create_hotkey_system(timer.clone());
+        } else {
+            keys.insert(config().split_key().into(), Action::Split);
+            keys.insert(config().reset_key().into(), Action::Reset);
+            keys.insert(config().undo_key().into(), Action::Undo);
+            keys.insert(config().skip_key().into(), Action::Skip);
+            keys.insert(config().pause_key().into(), Action::Pause);
+            keys.insert(config().undo_all_key().into(), Action::UndoAllPauses);
+            keys.insert(config().prev_key().into(), Action::PreviousComparison);
+            keys.insert(config().next_key().into(), Action::NextComparison);
+            keys.insert(
+                config().toggle_timing_method_key().into(),
+                Action::ToggleTimingMethod,
+            );
+        }
+        keys
+    }
+    fn handle_keypress(&mut self, key: Key) {
+        let action = if let Some(action) = self.keys.get(&key) {
+            action.clone()
+        } else {
+            return;
+        };
+        self.action(&action);
+    }
+    fn action(&mut self, action: &Action) {
+        match action {
+            Action::Split => self.split_or_start(),
+            Action::Pause => self.pause(),
+            Action::Reset => self.reset(),
+            Action::Undo => self.undo_split(),
+            Action::Skip => self.skip_split(),
+            Action::NextComparison => self.switch_to_next_comparison(),
+            Action::PreviousComparison => self.switch_to_previous_comparison(),
+            Action::HideComparison => self.turn_off_comparisons(),
+            Action::LayoutUp => self.layout.scroll_up(),
+            Action::LayoutDown => self.layout.scroll_down(),
+            Action::SaveSplits => config().save_splits(&self.read()),
+            Action::OpenSplits => {
+                if let Some(file) = pick_splits_file() {
+                    *self = WTimer::new_from_splits(file);
+                    config().save_config();
+                }
+            }
+            Action::ToggleTimingMethod => self.toggle_timing_method(),
+            Action::Quit => {} //self.skip_split(),
+            _ => unimplemented!("{:?}", action),
+        }
+    }
+}
+fn pick_splits_file() -> Option<PathBuf> {
+    FileDialog::new()
+        .add_filter("splits", &["lss", "rs"])
+        .set_directory(".")
+        .pick_file()
+}
+
+fn config() -> RwLockReadGuard<'static, Config> {
+    CONFIG.read().unwrap()
+}
+fn config_mut() -> RwLockWriteGuard<'static, Config> {
+    CONFIG.write().unwrap()
+}
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Hotkeys
+    #[arg(short, long)]
+    global_hotkeys: bool,
+    /// Splits file
+    #[arg(short, long)]
+    splits: Option<PathBuf>,
+    /// State file
+    #[arg(short, long)]
+    layout: Option<String>,
+    /// State file
+    #[arg(short, long)]
+    timing_file: Option<PathBuf>,
+}
+
+fn scroll_up(delta: &MouseScrollDelta) -> bool {
+    match delta {
+        MouseScrollDelta::LineDelta(y, ..) => {
+            return y < &0.0;
+        }
+        MouseScrollDelta::PixelDelta(winit::dpi::PhysicalPosition { y, .. }) => {
+            return y < &0.0;
+        }
+    }
+}
+
+fn key_code(input: &KeyboardInput, pressed: bool) -> Option<VirtualKeyCode> {
+    if input.state == ElementState::Pressed && pressed {
+        input.virtual_keycode
+    } else {
+        None
     }
 }
 
 fn main() {
-    let config = Config::parse("config.yaml").unwrap_or_default();
-    config.setup_logging();
-
-    let run = config.parse_run_or_default();
-
-    let timer = Timer::new(run).unwrap().into_shared();
-    config.configure_timer(&mut timer.write().unwrap());
-
-    let mut markers = config.build_marker_client();
-
-    let auto_splitter = auto_splitting::Runtime::new(timer.clone());
-    config.maybe_load_auto_splitter(&auto_splitter);
-
-    if config.use_global_hotkeys() {
-        let _hotkey_system = config.create_hotkey_system(timer.clone());
+    *config_mut() = Config::parse("config_save.yaml").unwrap_or_default();
+    config().setup_logging();
+    let args = Args::parse();
+    if args.global_hotkeys {
+        config_mut().set_use_global_hotkeys(args.global_hotkeys);
+    }
+    if let Some(ref val) = args.splits {
+        config_mut().set_splits_path(&val.to_path_buf());
+    }
+    if let Some(val) = args.timing_file {
+        config_mut().set_state_file(&val);
     }
 
-    let mut layout = config.parse_layout_or_default();
-    for comp in &mut layout.components {
-        match comp {
-            Component::Splits(s) => {
-                let settings = s.settings_mut();
-                //settings.fill_with_blank_space = false;
-                dbg!(&settings.fill_with_blank_space);
-            }
-            _ => {}
-        }
-    }
-    /*
-    if false {
-        layout.push(livesplit_core::component::graph::Component::new());
-        let mut layout_file = std::fs::File::create("layout.json").unwrap();
-        let settings = layout.settings();
-        settings.write_json(layout_file);
-    }
-     */
-    //let settings = layout.general_settings();
+    let file = config().splits_path();
+    let mut wtimer = if let Some(file) = file {
+        WTimer::new_from_splits(file)
+    } else {
+        WTimer::new()
+    };
 
-    let mut wtimer = WTimer(timer);
+    let event_loop = EventLoop::new();
+    let size = config().window_size();
 
-    let mut window = config.build_window().unwrap();
+    let size = winit::dpi::LogicalSize::new(size[0] as f64, size[1] as f64);
+    let window = winit::window::WindowBuilder::new()
+        .with_title("LiveSplit One")
+        .with_inner_size(size)
+        .build(&event_loop)
+        .unwrap();
 
     let mut renderer = Renderer::new();
     let mut layout_state = LayoutState::default();
+
+    wtimer.load_state();
+
+    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
+    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
     let mut buf = Vec::new();
 
-    wtimer.load_state(); //load_state(&mut timer.write().unwrap());
+    let mut modifiers: ModifiersState = ModifiersState::empty();
 
-    let fps: f64 = 40.0;
-    let spf: u64 = (1_000_000.0 * 1.0 / fps).floor() as u64;
-    dbg!(spf);
-    window.limit_update_rate(Some(std::time::Duration::from_micros(25_000)));
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+        match event {
+            Event::WindowEvent { event: ref ev, .. } => match ev {
+                WindowEvent::CloseRequested { .. } => control_flow.set_exit(),
+                WindowEvent::Resized(_size) => {}
+                WindowEvent::MouseWheel { delta, .. } => {
+                    if scroll_up(&delta) {
+                        wtimer.layout.scroll_up();
+                    } else {
+                        wtimer.layout.scroll_down();
+                    }
+                }
+                WindowEvent::KeyboardInput { input, .. } => {
+                    if let Some(key) = key_code(&input, true) {
+                        let key = Key::from(key, modifiers);
+                        wtimer.handle_keypress(key)
+                    }
+                }
+                WindowEvent::ModifiersChanged(mods) => {
+                    modifiers = mods.clone();
+                }
+                _ => {}
+            },
+            Event::RedrawRequested(_) => {
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width as usize, size.height as usize)
+                };
+                {
+                    let timer = wtimer.timer.read().unwrap();
+                    wtimer.markers.tick(&timer);
+                    wtimer
+                        .layout
+                        .update_state(&mut layout_state, &timer.snapshot());
+                }
+                renderer.render(&layout_state, [width as u32, height as u32]);
 
-    while window.is_open() {
-        if let Some((_, val)) = window.get_scroll_wheel() {
-            if val > 0.0 {
-                layout.scroll_up();
-            } else if val < 0.0 {
-                layout.scroll_down();
+                if buf.len() != width * height {
+                    buf.resize(width * height, 0);
+                }
+                transpose(
+                    bytemuck::cast_slice_mut(&mut buf),
+                    bytemuck::cast_slice(renderer.image_data()),
+                );
+                surface.set_buffer(&buf, width as u16, height as u16);
             }
+            _ => {}
         }
-
-        if window.is_key_pressed(Key::S, KeyRepeat::No)
-            && (window.is_key_down(Key::LeftCtrl) || window.is_key_down(Key::RightCtrl))
-        {
-            config.save_splits(&wtimer.0.read().unwrap());
-        }
-        if !config.use_global_hotkeys() {
-            if window.is_key_pressed(Key::Space, KeyRepeat::No) {
-                wtimer.split_or_start();
-            }
-            if window.is_key_pressed(Key::P, KeyRepeat::No) {
-                wtimer.pause();
-            }
-            if window.is_key_pressed(Key::R, KeyRepeat::No) {
-                wtimer.reset();
-            }
-            if window.is_key_pressed(Key::U, KeyRepeat::No) {
-                wtimer.undo_split();
-            }
-            if window.is_key_pressed(Key::S, KeyRepeat::No) {
-                wtimer.skip_split();
-            }
-            if window.is_key_pressed(Key::Left, KeyRepeat::No) {
-                wtimer.switch_to_previous_comparison();
-            }
-            if window.is_key_pressed(Key::Right, KeyRepeat::No) {
-                wtimer.switch_to_next_comparison();
-            }
-            if window.is_key_pressed(Key::Down, KeyRepeat::No) {
-                wtimer.turn_off_comparison();
-            }
-        }
-        let (width, height) = window.get_size();
-        if width != 0 && height != 0 {
-            {
-                let timer = wtimer.0.read().unwrap();
-                markers.tick(&timer);
-                layout.update_state(&mut layout_state, &timer.snapshot());
-            }
-            renderer.render(&layout_state, [width as _, height as _]);
-
-            buf.resize(width * height, 0);
-
-            transpose(
-                bytemuck::cast_slice_mut(&mut buf),
-                bytemuck::cast_slice(renderer.image_data()),
-            );
-        }
-        window.update_with_buffer(&buf, width, height).unwrap();
-    }
+        if let Event::RedrawRequested(_) = event {}
+        window.request_redraw();
+    });
 }
 
 pub fn transpose(dst: &mut [[u8; 4]], src: &[[u8; 4]]) {
